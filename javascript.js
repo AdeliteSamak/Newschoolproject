670962ee8f932d88bbfdf64397b5fb0ff2d8e562
adelite <adelasamakova@gmail.com>
2019-10-31 10:09:11 +0100

    nove

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
 console.log ("ok")
+let login = "MrVerycool";
+let pw = "noonewilleverguest";
+
+if pw = "noonewilleverguest"; {
+    console.log ("You're in");
+}
+else {
+    console.log ("You're out");
+}
